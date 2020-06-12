5b74dce51ecfa8b74d338bfd0a7f6b0c1e4d050e
qp <2457476273@qq.com>
2020-06-12 16:15:49 +0800

    six commit

--- src/main/java/com/example/demo/nowcoder/Solution10.java
+++ src/main/java/com/example/demo/nowcoder/Solution10.java
@@ -7,11 +7,24 @@ package com.example.demo.nowcoder;
  * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
  * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
  * 比如n=3时，2*3的矩形块有3种覆盖方法：
+ * 递归求解 当target=1 是 1 target=2 2
+ * target=3 3 target>2
+ * f(n)=f(n-1)+f(n-2)
  */
 public class Solution10 {
     public static void main(String[] args) {
-
+        System.out.println(RectCover(4));
     }
 
+    public static int RectCover(int target) {
+       // target=1 1 target=2
+        if (target<1) {
+            return 0;
+        }else if (target<=2) {
+            return target;
+        }else {
+            return RectCover(target-1)+RectCover(target-2);
+        }
 
+    }
 }
